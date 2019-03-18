c0ee2f74e28b0584020ff87fdf934414d2d06899
mlefter <m.lefter@lumc.nl>
2019-03-18 14:48:13 +0100

    Add sequence retrieval

--- retriever/parsers/gff_other.py
+++ retriever/parsers/gff_other.py
@@ -27,36 +27,87 @@ from BCBio.GFF import GFFParser
 import io
 import requests
 import json
+from Bio import Entrez
+from Bio import SeqIO
+from retriever.validation import validate
 
 
-def extract_model(feature):
+def get_qualifiers(feature):
+    qualifiers = {}
+    for qualifier in feature.qualifiers:
+        if len(feature.qualifiers[qualifier]) == 1:
+            qualifiers[qualifier] = feature.qualifiers[qualifier][0]
+        else:
+            qualifiers[qualifier] = feature.qualifiers[qualifier]
+    return qualifiers
+
+
+def extract_raw_features(feature):
     model = {'id': feature.id,
-             'type': feature.type}
+             'type': feature.type,
+             'qualifiers': get_qualifiers(feature),
+             'start': {'position': feature.location.start},
+             'end': {'position': feature.location.end},
+             'orientation': feature.location.strand}
     if feature.sub_features:
         model['sub_features'] = []
         for sub_feature in feature.sub_features:
-            model['sub_features'].append(extract_model(sub_feature))
+            model['sub_features'].append(extract_raw_features(sub_feature))
     return model
 
 
-def parse(feature_id):
-    """
-    Entry point for the GFF parser.
-    """
+def get_raw_record(feature_id):
     URL = 'https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi'
-    payload = {'db': 'nuccore',
-               'report': 'gff3',
-               'id': feature_id}
-    response = requests.get(URL, params=payload)
+    params = {'db': 'nuccore',
+              'report': 'gff3',
+              'id': feature_id}
 
-    gff_parser = GFFParser()
-    gff = gff_parser.parse(io.StringIO(response.text))
+    try:
+        response = requests.get(URL, params=params)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Connection Error:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("Some other Error", err)
+    else:
+        gff_parser = GFFParser()
+        gff = gff_parser.parse(io.StringIO(response.text))
 
-    model = []
-    for rec in gff:
-        for feature in rec.features:
-            model.append(extract_model(feature))
-    print(json.dumps(model, indent=2))
+        model = []
+        for rec in gff:
+            for feature in rec.features:
+                model.append(extract_raw_features(feature))
+        return model
+
+
+def get_id(feature):
+    if feature.type == 'CDS':
+        return feature.qualifiers.get('protein_id')[0]
+    elif feature.type in ['mRNA']:
+        return feature.qualifiers.get('transcript_id')[0]
+    elif feature.type == 'gene':
+        for dbxref in feature.qualifiers['Dbxref']:
+            if dbxref.startswith('HGNC'):
+                return dbxref.split(':')[-1]
+    else:
+        return feature.id
+
+
+def get_sequence(feature_id):
+
+    Entrez.email = 'm.lefter@lumc.nl'
+
+    handle = Entrez.efetch(db='nucleotide', id=feature_id, rettype='fasta')
+    records = []
+    for record in SeqIO.parse(handle, "fasta"):
+        records.append(str(record.seq))
+    handle.close()
+    if len(records) == 1:
+        return records[0]
 
 
 def main():
@@ -72,7 +123,7 @@ def main():
 
     args = parser.parse_args()
 
-    parse(args.id)
+    get_raw_record(args.id)
 
 
 if __name__ == '__main__':
