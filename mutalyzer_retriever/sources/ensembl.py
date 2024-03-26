c88ba4757132f31b23edc57f025cbe63aeb64bb2
Xiaoyun Liu <x.liu@lumc.nl>
2024-03-26 17:21:41 +0100

    fix retrive order: fetch from tark directly

--- mutalyzer_retriever/sources/ensembl.py
+++ mutalyzer_retriever/sources/ensembl.py
@@ -155,16 +155,13 @@ def get_api_base(r_id, r_version, transcript = False):
         
     if transcript:
         tark_versions_38, tark_versions_37 = _get_tark_versions(r_id,settings.get("ENSEMBL_TARK_API"))
-        if r_version in [None, rest_version_38]:
-            return settings.get("ENSEMBL_API"), "GRCh38"
+        if not r_version:
+            return settings.get("ENSEMBL_TARK_API"), "GRCh38"
         elif r_version in tark_versions_38:
             return settings.get("ENSEMBL_TARK_API"), "GRCh38"
-        elif r_version == rest_version_37:
-            return settings.get("ENSEMBL_API_GRCH37"), "GRCh37"
         elif r_version in tark_versions_37:
             return settings.get("ENSEMBL_TARK_API"), "GRCh37"
-    else:
-        raise ValueError(f"Cannot fetch {r_id} with version {r_version} from Ensembl")
+    raise ValueError(f"Cannot fetch {r_id} with version {r_version} from Ensembl")
 
 
 
@@ -184,10 +181,8 @@ def fetch(reference_id, reference_type=None, timeout=1):
         return fetch_json(r_id,api_base), "json"  
     elif reference_type in [None,"json"] and "ENST" in r_id:
         api_base, assembly = get_api_base(r_id, r_version, transcript=True)
-        if api_base in [settings.get("ENSEMBL_API"), settings.get("ENSEMBL_API_GRCH37")]:
-            return fetch_json(r_id,api_base), "json"
-        elif api_base == settings.get("ENSEMBL_TARK_API"):
-            return fetch_tark(r_id,api_base,assembly), "json"
+        print(api_base,assembly)
+        return fetch_tark(r_id,api_base,assembly), "json"
    
     elif reference_type == "genbank":
         return None, "genbank"
