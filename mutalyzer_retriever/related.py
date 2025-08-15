a1809eb5be2a08fe35d402bb8503b0d6e1c8a13f
Xiaoyun Liu <x.liu@lumc.nl>
2025-08-15 09:55:55 +0200

    WIP: local changes before merging master

--- mutalyzer_retriever/related.py
+++ mutalyzer_retriever/related.py
@@ -25,7 +25,7 @@ def _fetch_ncbi_elink(db, dbfrom, query_id, timeout=10):
 
 
 def _fetch_ncbi_datasets_gene_accession(accession_id, timeout=1):
-    url = f"https://api.ncbi.nlm.nih.gov/datasets/v1/gene/accession/{accession_id}"
+    url = f"https://api.ncbi.nlm.nih.gov/datasets/v2/gene/accession/{accession_id}"
     return json.loads(request(url=url, timeout=timeout))
 
 
@@ -65,14 +65,14 @@ def _get_summary_accession_versions(summary):
     return output
 
 
-def _get_new_versions(summary, timeout):
+def _get_new_versions(summary, timeout, db="nucleotide"):
     new_versions = set()
     if summary.get("replacedby"):
         new_versions.add(summary["replacedby"])
         new_versions.update(
             _get_new_versions(
                 _get_summary_result_one(
-                    _fetch_ncbi_esummary("nucleotide", summary["replacedby"], timeout)
+                    _fetch_ncbi_esummary(db, summary["replacedby"], timeout)
                 ),
                 timeout,
             )
