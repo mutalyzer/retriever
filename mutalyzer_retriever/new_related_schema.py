15ca46c4eeb422aaa3b8af5774af0f4fe79aa48d
Xiaoyun Liu <x.liu@lumc.nl>
2025-08-25 13:39:21 +0200

    Move ensembl and ncbi id for genes under gene schema.

--- mutalyzer_retriever/new_related_schema.py
+++ mutalyzer_retriever/new_related_schema.py
@@ -1,28 +1,22 @@
 from schema import Schema, And, Or, Optional
 
-gene_provider_schema = Schema({
-    "name": And(str, lambda s: s.strip() != ""),
-    Optional("accession"): And(str, lambda s: s.strip() != "")
-})
-
 transcript_provider_schema = Schema({
     "name": And(str, lambda s: s.strip() != ""),
     Optional("transcript_id"): And(str, lambda s: s.strip() != ""),
     Optional("protein_id"): And(str, lambda s: s.strip() != ""),
-    Optional("transcript_description"): And(str, lambda s: s.strip() != ""),
-    Optional("protein_description"): And(str, lambda s: s.strip() != "")
+    Optional("description"): And(str, lambda s: s.strip() != ""),
 })
 
 transcript_schema = Schema({
     Optional("tag"): And(str, lambda s: s.strip() != ""),
     "providers": [transcript_provider_schema],
-    Optional("description"): And(str, lambda s: s.strip() != "")
 })
 
 gene_schema = Schema({
     Optional("hgnc_id"): And(str, lambda s: s.strip() != ""),
+    Optional("ensembl_id"): And(str, lambda s: s.strip() != ""),
+    Optional("ncbi_rsg_id"): And(str, lambda s: s.strip() != ""),
     "name": And(str, lambda s: s.strip() != ""),
-    "providers": [gene_provider_schema],
     "transcripts": [transcript_schema],
     Optional("description"): And(str, lambda s: s.strip() != "")
 })
