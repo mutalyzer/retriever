6f57ca590a1ed12712420c64fe30ee18c530e7b5
Xiaoyun Liu <x.liu@lumc.nl>
2025-08-27 11:44:19 +0200

    Add providers under gene.

--- mutalyzer_retriever/new_related_schema.py
+++ mutalyzer_retriever/new_related_schema.py
@@ -12,12 +12,17 @@ transcript_schema = Schema({
     "providers": [transcript_provider_schema],
 })
 
+gene_provider_schema = Schema({
+    "name": And(str, lambda s: s.strip() != ""),
+    "accession": And(str, lambda s: s.strip() != "")
+})
+
 gene_schema = Schema({
     Optional("hgnc_id"): And(str, lambda s: s.strip() != ""),
-    Optional("ensembl_id"): And(str, lambda s: s.strip() != ""),
-    Optional("ncbi_rsg_id"): And(str, lambda s: s.strip() != ""),
+    Optional("refseqgene"): And(str, lambda s: s.strip() != ""),
     "name": And(str, lambda s: s.strip() != ""),
-    "transcripts": [transcript_schema],
+    Optional("providers"): [gene_provider_schema],
+    Optional("transcripts"): [transcript_schema],
     Optional("description"): And(str, lambda s: s.strip() != "")
 })
 
