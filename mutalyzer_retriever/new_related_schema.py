2699ff366c766c132e46cc1f765c1548d274ea5b
Xiaoyun Liu <x.liu@lumc.nl>
2025-09-03 11:25:33 +0200

    Rename ID to accession.

--- mutalyzer_retriever/new_related_schema.py
+++ mutalyzer_retriever/new_related_schema.py
@@ -2,13 +2,13 @@ from schema import Schema, And, Or, Optional
 
 transcript_provider_schema = Schema({
     "name": And(str, lambda s: s.strip() != ""),
-    Optional("transcript_id"): And(str, lambda s: s.strip() != ""),
-    Optional("protein_id"): And(str, lambda s: s.strip() != ""),
+    Optional("transcript_accession"): And(str, lambda s: s.strip() != ""),
+    Optional("protein_accession"): And(str, lambda s: s.strip() != ""),
     Optional("description"): And(str, lambda s: s.strip() != ""),
 })
 
 transcript_schema = Schema({
-    Optional("tag"): And(str, lambda s: s.strip() != ""),
+    Optional("tag"): [And(str, lambda s: s.strip() != "")],
     "providers": [transcript_provider_schema],
 })
 
